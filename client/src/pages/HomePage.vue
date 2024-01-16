c427130eba4fc068b541dde602cc93855994b6ec
Mick <beasticorn@gmail.com>
2024-01-16 09:34:46 -0700

    sick filter

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -5,18 +5,31 @@
   </section>
  </div>
  <div class="container">
+<!-- SECTION filter bar -->
+  <section class="row bg-info p-3 rounded my-2 gap-2">
+<!-- ['dogs', 'cats', 'games', 'gachamon', 'misc', 'asthetics', 'animals'] -->
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == ''}" @click="filterBy = ''">all</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'cats'}" @click="filterBy = 'cats'">cats</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'dogs'}" @click="filterBy = 'dogs'">dogs</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'games'}" @click="filterBy = 'games'">games</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'gachamon'}" @click="filterBy = 'gachamon'">gachamon</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'aesthetics'}" @click="filterBy = 'aesthetics'">aesthetics</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'animals'}" @click="filterBy = 'animals'">animals</button>
+    <button class="col btn btn-outline-light" :class="{'bg-primary': filterBy == 'misc'}" @click="filterBy = 'misc'">misc</button>
+  </section>
   <section class="row">
     <!-- {{ albums }} -->
-    <div class="col-3" v-for="album in albums" :key="album.id">
+    <div class="col-3" v-for="album in albums" :key="album.id" v-if="albums.length">
       <!-- {{ album.title }} -->
       <AlbumCard :album="album"/>
     </div>
+    <div v-else> no albums here 🤷</div>
   </section>
  </div>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { AppState } from '../AppState.js';
 import AlbumCard from '../components/AlbumCard.vue';
 import { albumsService } from '../services/AlbumsService.js';
@@ -26,6 +39,7 @@ export default {
     onMounted(()=> {
       getAllAlbums()
     })
+    const filterBy = ref('')
     async function getAllAlbums(){
       try {
         await albumsService.getAllAlbums()
@@ -34,7 +48,14 @@ export default {
       }
     }
     return {
-      albums: computed(()=> AppState.albums)
+      filterBy,
+      albums: computed(()=> {
+        if(filterBy.value){
+          return AppState.albums.filter(a => a.category == filterBy.value)
+        } else {
+          return AppState.albums
+        }
+      })
     }
   },
   components: {AlbumCard}
